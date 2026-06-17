81ffae0a2711a4726e7929a42b2b8d4fd6937a60
davideros3 <david.eross3@gmail.com>
2026-06-17 19:56:23 +0200

    published on npm

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,41 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url'; 
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), cssInjectedByJsPlugin()],
-  
 
   test: {
     globals: true,
-    environment: 'jsdom', 
-    setupFiles: './vitest.setup.js',
-    css: true, 
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.js",
+    css: true,
   },
-
 
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'ReactCustomDatepicker',
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "ReactCustomDatepicker",
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react/jsx-runtime': 'jsxRuntime', 
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "jsxRuntime",
         },
       },
     },
+    emptyOutDir: true,
+    copyPublicDir: false,
+    minify: true,
   },
 });
